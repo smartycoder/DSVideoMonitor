b2db7ea353e658e6521dd964f721cf31a48697fa
smartycoder <mato46@gmail.com>
2016-03-14 09:35:05 +0100

    Update PushNotificationClient.py

--- notification/PushNotificationClient.py
+++ notification/PushNotificationClient.py
@@ -1,10 +1,11 @@
 from abc import ABCMeta, abstractmethod
-
+from logging import Logger
 
 class PushNotificationClient():
     __metaclass__ = ABCMeta
 
     _api_key = None
+    _logger = Logger
 
     @abstractmethod
     def set_api_key(self, api):
@@ -13,3 +14,7 @@ class PushNotificationClient():
     @abstractmethod
     def send_message(self, title, msg):
         pass
+
+    @abstractmethod
+    def set_logger(self, logger):
+        self._logger = logger
