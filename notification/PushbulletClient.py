ede6c7cc1f7c25b01c997f45e4536405740a17eb
smartycoder <mato46@gmail.com>
2016-03-14 09:18:10 +0100

    Update PushbulletClient.py

--- notification/PushbulletClient.py
+++ notification/PushbulletClient.py
@@ -7,9 +7,15 @@ class PushbulletClient(PushNotificationClient):
     _client = None
 
     def set_api_key(self, api):
-        self._client = PushBullet(api)
+        try:
+            self._client = PushBullet(api)
+        except Exception as e:
+            self._logger.exception(str(e))
 
     def send_message(self, title, msg):
-        self._client.push_note(title=title, body=msg)
+        try:
+            self._client.push_note(title=title, body=msg)
+        except Exception as e:
+            self._logger.exception(str(e))
 
 
