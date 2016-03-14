0386678094fe1815f51428763c56d3db9ab74ddc
smartycoder <mato46@gmail.com>
2016-03-14 09:18:36 +0100

    Update PushoverClient.py

--- notification/PushoverClient.py
+++ notification/PushoverClient.py
@@ -7,7 +7,13 @@ class PushoverClient(PushNotificationClient):
     _api_token = "aYBH7B28vQCKopoJXGCnwQ5NhPTG9w"
 
     def set_api_key(self, api):
-        self._client = Client(user_key=api, api_token=self._api_token)
+        try:
+            self._client = Client(user_key=api, api_token=self._api_token)
+        except Exception as e:
+            self._logger.exception(str(e))
 
     def send_message(self, title, msg):
-        self._client.send_message(title=title, message=msg)
+        try:
+            self._client.send_message(title=title, message=msg)
+        except Exception as e:
+            self._logger.exception(str(e))
